30cb50a879deb9518a442635e1d14437eb3b4cb5
Fernando Herrera <fernando.herrera85@gmail.com>
2023-09-05 11:08:08 -0400

    POSTGRES_URL env added

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,6 +6,7 @@ export const envs = {
 
   PORT: get('PORT').required().asPortNumber(),
   PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
+  POSTGRES_URL: get('POSTGRES_URL').default('public').asString(),
 
 }
 
